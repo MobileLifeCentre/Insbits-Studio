7537125cac2e1d346bc49622d0da6ed7e31c1a77
Victor Guerrero Corbi <guerre50@gmail.com>
2014-02-19 19:19:48 +0100

    Shell script installer
    Tested on MEAN stack and Ubuntu using Digital Ocean machine

---
+++ rflea.sh
@@ -0,0 +1,109 @@
+#!/bin/bash
+# rflea installer variables
+SPACEBREW_GIT=https://github.com/MobileLifeCentre/spacebrew.git
+NODERED_GIT=https://github.com/MobileLifeCentre/node-red.git
+INSTALL_DIRECTORY=/var/www
+SPACEBREW_SERVICE_DIRECTORY=/etc/init/spacebrew.conf
+NODERED_SERVICE_DIRECTORY=/etc/init/node-red.conf
+
+# functions
+install(){
+        cd $INSTALL_DIRECTORY
+
+        echo "Downloading Spacebrew \n"
+        git clone $SPACEBREW_GIT
+
+        echo "Downloading Node-red \n"
+        git clone $NODERED_GIT
+
+        echo "Installing Spacebrew \n"
+        cd ${INSTALL_DIRECTORY}/spacebrew
+        npm install ws
+        npm install firebase
+
+        echo "Installing Node-red \n"
+        cd ${INSTALL_DIRECTORY}/node-red
+        npm install
+
+        echo "Setting services \n"
+        cd $INSTALL_DIRECTORY
+        cp ./spacebrew/spacebrew.conf $SPACEBREW_SERVICE_DIRECTORY
+        chmod +x /etc/init/spacebrew.conf
+        cp ./node-red/node-red.conf $NODERED_SERVICE_DIRECTORY
+        chmod +x /etc/init/node-red.conf
+
+        echo "Starting services"
+        start spacebrew
+        start node-red
+
+        echo "Install finished"
+}
+uninstall() {
+        echo "Stopping services"
+        stop spacebrew
+        stop node-red
+        cd $INSTALL_DIRECTORY
+        echo "Removing files \n"
+        rm -R ${INSTALL_DIRECTORY}/node-red
+        rm -R ${INSTALL_DIRECTORY}/spacebrew
+        rm /etc/init/node-red.conf
+        rm /etc/init/spacebrew.conf
+
+        echo "Uninstall finished"
+}
+update_spacebrew() {
+        echo "Updating spacebrew"
+        cd ${INSTALL_DIRECTORY}/spacebrew
+        git pull
+        restart spacebrew
+}
+
+update_nodered() {
+        echo "Updating node-red"
+        cd ${INSTALL_DIRECTORY}/node-red
+        git pull
+        restart node-red
+}
+
+update() {
+        if [ "$2" = "spacebrew" ]
+        then
+                update_spacebrew
+        elif [ "$2" = "node-red" ]
+        then
+                update_nodered
+        else
+                update_spacebrew
+                update_nodered
+        fi
+
+        echo "Update finished"
+
+}
+
+print_instructions() {
+        echo "This are the available options:\n"
+        echo " install\n"
+        echo " update [spacebrew|node-red] \n"
+        echo " uninstall \n"
+}
+
+# logic
+initial_directory=`pwd`
+echo $initial_directory
+
+if [ "$1" = 'install' ]
+then
+        install
+elif [ "$1" = 'update' ]
+then
+        update
+elif [ "$1" = 'uninstall' ]
+then
+        uninstall
+else
+        print_instructions
+fi
+
+cd $initial_directory
+exit 1
